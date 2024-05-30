f798db919d05a473ee4667a76f5f179b0bf3d478
paisible7 <22km127@esisalama.org>
2024-05-30 13:38:25 +0200

    COMMIT30MAI = portfolio basic ok

--- js/script.js
+++ js/script.js
@@ -64,7 +64,30 @@ backProfileBnt.onclick = () => {
 }
 
 const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector(".book-page.page-left");
 
 setTimeout(() => {
     coverRight.classList.add('turn');
 }, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+            
+
+        }, 500)
+    }, (index + 1) * 200 + 2100)
+})
