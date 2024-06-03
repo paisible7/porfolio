efb4cf70f147181549f444dfd2f501c245e91164
paisible7 <22km127@esisalama.org>
2024-06-03 12:50:33 +0200

    COMMIT3JUIN = ajout du responsive

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
 pageTurnBtn.forEach((el, index) => {
     el.onclick = () => {
-        const pageTurnId = el.getAttribute('date-page');
+        const pageTurnId = el.getAttribute('data-page');
         const pageTurn = document.getElementById(pageTurnId);
 
         if (pageTurn.classList.contains('turn')){
