37925fe13bbd17fb430a5d9a7d3f77abf23fb126
paisible7 <22km127@esisalama.org>
2024-08-21 15:22:05 +0200

    modified

--- js/annonce.js
+++ js/annonce.js
@@ -9,7 +9,7 @@
         mm = String(journee.getMonth() + 1).padStart(2, "0"),
         yyyy = journee.getFullYear(),
         annee = yyyy + 1,
-        jrMois = "08/20/",
+        jrMois = "08/25/",
         jourJ = jrMois + yyyy;
     
     journee = mm + "/" + dd + "/" + yyyy;
