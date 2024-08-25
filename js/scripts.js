2cf3b50500e4556d511c179ef1f08985c015586b
paisible7 <22km127@esisalama.org>
2024-08-25 15:43:42 +0200

    COMMIT_25_08_2024 ==> ajout des skills et de la section à propos

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 const imgs = document.querySelectorAll('.img_item');
 let dernierIndex = 0;
-let isHovering = false;
+let survol = false;
 
 function changeImage() {
-  if (!isHovering) {
-    imgs.forEach(div => div.classList.remove('active'));
-    imgs[dernierIndex].classList.add('active');
-    dernierIndex = (dernierIndex + 1) % imgs.length;
-  }
+    if (!survol) {
+        imgs.forEach(div => div.classList.remove('active'));
+        imgs[dernierIndex].classList.add('active');
+        dernierIndex = (dernierIndex + 1) % imgs.length;
+    }
 }
 
 changeImage();
 setInterval(changeImage, 5500);
 
 imgs.forEach(img => {
-  img.addEventListener('mouseover', () => {
-    isHovering = true;
-  });
-  img.addEventListener('mouseout', () => {
-    isHovering = false;
-  });
+    img.addEventListener('mouseover', () => {
+        survol = true;
+    });
+    
+    img.addEventListener('mouseout', () => {
+        survol = false;
+    });
 });
