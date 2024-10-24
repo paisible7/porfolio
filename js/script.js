b0e19cc3524aa887d141f00c7d587405814ecdf8
paisible7 <22km127@esisalama.org>
2024-10-24 08:19:11 +0200

    COMMIT_24_OCTOBRE_2024 ==> modification du textearea et ajustement des liens

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navlink = document.querySelector('.nav');
+// let btn = document.getElementById('btn')
 
 menuIcon.onclick = () => {
     navlink.classList.toggle('active');
 }
+
+// btn.addEventListener('click', () => {
+//     let a = "https://github.com/paisiible7"
+//     document.createElement
+// })
