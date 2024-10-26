f74dd0ef698f8fa82124a91f7d9670a10f7f74d5
paisible7 <22km127@esisalama.org>
2024-10-26 17:16:02 +0200

    COMMIT_26_OCTOBRE_2024 ==> configuration de la reponse automatique ok

--- js/mail.js
+++ js/mail.js
@@ -5,18 +5,45 @@ function sendMail() {
 
     var serviceID = 'service_ljg3pyd';
     var templateID = 'template_nqzm3ib';
+    var autoReplyTemplateID = 'template_6hqzhrn';
 
     var params = {
         sendername: document.getElementById('name').value,
         senderemail: document.getElementById('email').value,
         message: document.getElementById('message').value,
-        subject: "MESSAGE DE CONTACT"
+        subject: 'MESSAGE DE CONTACT'
     }
 
-    emailjs.send(serviceID, templateID, params, this)
-    .then( res => {
-        alert('Merci, ' + params['sendername']  + ' ! Votre message a été envoyé avec succès.')
-    })
-
-    .catch()
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            emailjs.send(serviceID, autoReplyTemplateID, {
+                sendername: params.sendername,
+                senderemail: params.senderemail,
+                message: params.message,
+            })
+                .then(res => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Succès!',
+                        text: 'Merci, ' + params['sendername'] + '! Votre message a été envoyé avec succès.',
+                        confirmButtonText: 'OK'
+                    });
+                })
+                .catch(err => {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Erreur !',
+                        text: "Une erreur est survenue lors de l'envoi de la réponse",
+                        confirmButtonText: 'OK'
+                    });
+                });
+        })
+        .catch(err => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Erreur !',
+                text: "Une erreur est survenue lors de l'envoi.",
+                confirmButtonText: 'OK'
+            });
+        });
 }
